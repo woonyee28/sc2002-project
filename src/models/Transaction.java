e0a2a93164341b06efc57905957dd43f7274b191
woonyee28 <ngnwy289@gmail.com>
2022-10-23 20:09:01 +0800

    update transaction with movie id2

---
+++ src/models/Transaction.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Transaction {
+    private String TID;
+    private int ;
+    private String synopsis;
+    private int rating;
+    private String showingStatus;
+    private String director;
+}
