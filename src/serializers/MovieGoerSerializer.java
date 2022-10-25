84239374315919622849f62150ada50a52c5878f
woonyee28 <ngnwy289@gmail.com>
2022-10-25 23:43:35 +0800

    movie goer serializer

--- src/serializers/MovieGoerSerializer.java
+++ src/serializers/MovieGoerSerializer.java
@@ -69,5 +69,73 @@ public class MovieGoerSerializer {
         }
         return null;
     }
+
+    public static void overwriteMovieGoerCSV(ArrayList<MovieGoer> aList) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("database/movieGoersData.csv",false)));
+			for(MovieGoer movieGoer:aList) {
+                StringBuffer oneLine = new StringBuffer();
+                oneLine.append(movieGoer.getMovieGoersID());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getName());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getEmail());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getAge());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getPasswordHashed());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getMobileNumber());
+                oneLine.append(CSV_SEPARATOR);
+                oneLine.append(movieGoer.getTID_List());
+    
+                bw.write(oneLine.toString());
+                bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+		}
+		catch (UnsupportedEncodingException e) {System.out.printf("'%s' %n", "Unsupported Encoding");}
+        catch (FileNotFoundException e){System.out.printf("'%s' %n", "File Not Found"); }
+        catch (IOException e){e.printStackTrace();}
+	
+    }
+
+    public static void updateMovieGoerFromCSV(int movieGoersID, String name, String email, int age, String passwordHashed, int mobileNumber, String TID_List) {
+	    ArrayList<MovieGoer> aList = MovieGoerSerializer.readFromMovieGoerCSV();
+	    int flag =0;
+	    for (MovieGoer a:aList) {
+		    if (a.getName().equals(name) && a.getEmail().equals(email)) {
+		    	a.setMovieGoersID(movieGoersID);
+                a.setAge(age);
+                a.setpasswordHashed(passwordHashed);
+                a.setMobileNumber(mobileNumber);
+                a.setTID_List(TID_List);
+		    	flag=1;
+		    	break;
+	    	}
+	    }
+	    if (flag==1){
+	        MovieGoerSerializer.overwriteMovieGoerCSV(aList);
+	        System.out.println("MovieGoer record, name = " + name +" successfully updated!");
+	    } else System.out.println("MovieGoer record, name " + name+" update unsuccessful!");
+    }
+
+    public static void deleteMovieGoerFromCSV(String name, String email) {
+        ArrayList<MovieGoer> aList = MovieGoerSerializer.readFromMovieGoerCSV();
+        int index=0,flag=0;
+        for (MovieGoer a:aList) {
+            if (a.getName().equals(name) && a.getEmail().equals(email)) {
+                flag=1;
+                break;
+            }
+            index++;
+        }
+        if (flag==1) {
+            aList.remove(index);
+            MovieGoerSerializer.overwriteMovieGoerCSV(aList);
+            System.out.println("MovieGoer record, name = " + name +" successfully updated!");
+	    } else System.out.println("MovieGoer record, name " + name+" update unsuccessful!");
+    }
 }
  
