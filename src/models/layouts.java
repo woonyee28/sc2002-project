3ae81b63f28f8054458073cf2c9962d10aced8b3
Ng Zheng Kai <102434032+Nzkai@users.noreply.github.com>
2022-10-25 16:01:35 +0800

    Add files via upload

---
+++ src/models/layouts.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class layouts {
+	
+	private ArrayList<MovieSeat> seats;
+	private int row; 
+	private int rowType;
+	private int TotalSeats;
+
+	//rowType is Plat-gold,regular class
+	public layouts (int rowType, int TotalSeats, int row)
+	{
+		this.rowType = rowType;
+		this.TotalSeats = TotalSeats;
+		this.row = row;
+		//create array lists of Movieseats
+		seats = new ArrayList<MovieSeat>();
+		//Create the seats
+		createSeats(this.TotalSeats);
+	}
+	
+	public void setRowType(int rowType)
+	{
+		this.rowType = rowType;
+	}
+
+    public int getRowNumber()
+	{
+		return this.row;
+	}
+	
+	public int getRowType()
+	{
+		return this.rowType;
+	}
+	
+	public void createSeats(int TotalSeats)
+	{
+		for (int i = 1; i <= TotalSeats; i++)
+		{
+			seats.add(new MovieSeat(i));
+		}
+	}
+	
+	public ArrayList<MovieSeat> getSeats()
+	{
+		return seats;
+	}
+}
