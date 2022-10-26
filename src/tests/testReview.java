250d7988d0eb4ff55913d7a3443f639654931b1b
Woon Yee Ng <ngnwy289@gmail.com>
2022-10-26 14:38:33 +0800

    update review ser

--- src/tests/testReview.java
+++ src/tests/testReview.java
@@ -6,10 +6,12 @@ import serializers.ReviewSerializer;
 public class testReview {
     public static void main(String[] args) {
         Review review = new Review(0,0,4.9,"Nice movie!",0);
-        ReviewSerializer.writeToReviewCSV(review);
-        for (Review m: ReviewSerializer.readFromReviewCSV()) {           
-            m.toString();
-            System.out.println(m); 
-        }
+        // ReviewSerializer.writeToReviewCSV(review);
+        // for (Review m: ReviewSerializer.readFromReviewCSV()) {           
+        //     m.toString();
+        //     System.out.println(m); 
+        // }
+        ReviewSerializer.deleteReviewFromCSV(0);
+        ReviewSerializer.updateReviewFromCSV(0,0,4.9,"Nice changed movie!",0);
     }
 }
