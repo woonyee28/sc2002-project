ebee7e9ef7919cc4a93b0a5f9f6ef918304397e1
Woon Yee Ng <ngnwy289@gmail.com>
2022-10-31 14:26:30 +0800

    session manager half way

--- src/managers/SessionManager.java
+++ src/managers/SessionManager.java
@@ -12,10 +12,23 @@ public class SessionManager {
     }
 
     public static void createNewSession(){
-
+        MovieManager.printAllMovies();
+        Scanner ii = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Movie Id: ");
+        int movieID = ii.nextInt();
+        System.out.println("Enter Session Date YYYYMMDD: ");
+        String sessionDate = sc.nextLine();
+        System.out.println("Enter Session Time HHMM: ");
+        String sessionTime = sc.nextLine();
+        ArrayList<Integer> seatingPlan = new ArrayList<Integer>();
+        seatingPlan.add(-1);
+        Sessions sess = new Sessions(movieID, sessionDate, sessionTime, seatingPlan);
+        SessionSerializer.writeToSessionCSV(sess);
     }
     
     public static void modifySession(){
 
     }
 }
+
