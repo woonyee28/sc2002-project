8e84a12172235a467e66a9655408b81d00eb8880
Dingang123 <102493330+Dingang123@users.noreply.github.com>
2022-11-07 23:48:42 +0800

    s

--- src/tests/testBooking.java
+++ src/tests/testBooking.java
@@ -5,14 +5,21 @@ import managers.*;
 public class testBooking {
     public static void main(String [] args){
         while(true){
+        SettingsManager sm = new SettingsManager(1);
         MovieBookingManager mbm = new MovieBookingManager(1, 0);
-        try {
-            mbm.bookings(0);
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        sm.printPriceList();
+        sm.printHolidayList();
+        //20221201 weekday
+        //20221128 for holiday
+        //20221126 weekend
+            try {
+                mbm.bookings(0);
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+    
         }
     }
-    
 }
--- src/tests/testHolPriceConfig.java
+++ src/tests/testHolPriceConfig.java
@@ -6,8 +6,8 @@ public class testHolPriceConfig {
     public static void main(String[] args){
         SettingsManager sm = new SettingsManager(1);
         sm.printPriceList();
-        sm.editPrices();
         sm.printHolidayList();
+        sm.addHoliday(); //20221128 for new holiday
         MovieBookingManager mbm = new MovieBookingManager(1, 0);
         try {
             mbm.bookings(1);
